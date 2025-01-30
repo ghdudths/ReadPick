aa5bb5b3309259947d26fe72fbd06eb4b7b19534
ghdudths <ghdudths1117@naver.com>
2025-01-30 16:31:51 +0900

    로그인 완료

--- src/main/java/com/portfolio/ReadPick/dao/UserMapper.java
+++ src/main/java/com/portfolio/ReadPick/dao/UserMapper.java
@@ -1,8 +1,16 @@
 package com.portfolio.ReadPick.dao;
 
+import java.util.List;
+
 import org.apache.ibatis.annotations.Mapper;
+
+import com.portfolio.ReadPick.vo.UserVo;
 
 @Mapper
 public interface UserMapper {
+
+    List<UserVo> selectList();
+
+    UserVo selectOneFromId(String id);
 
 }

aa25a807c03b4c66e5f5b914df70fe34b5f1967d
ghdudths <ghdudths1117@naver.com>
2025-01-30 16:26:15 +0900

    오류수정

--- src/main/java/com/portfolio/ReadPick/dao/UserMapper.java
+++ src/main/java/com/portfolio/ReadPick/dao/UserMapper.java
@@ -1,13 +1,8 @@
 package com.portfolio.ReadPick.dao;
 
-import java.util.List;
-
 import org.apache.ibatis.annotations.Mapper;
-
-import com.portfolio.ReadPick.vo.UserVo;
 
 @Mapper
 public interface UserMapper {
-
 
 }

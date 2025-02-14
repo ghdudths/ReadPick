c15511c36a88458bfb705d5c3a9508b01cfe6441
ghdudths <ghdudths1117@naver.com>
2025-02-14 22:30:36 +0900

    02 14 22 30
    리액트 연동 설정클래스 추가

---
+++ src/main/java/com/portfolio/ReadPick/WebConfig.java
@@ -0,0 +1,18 @@
+package com.portfolio.ReadPick;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000") // 리액트 애플리케이션 주소
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
